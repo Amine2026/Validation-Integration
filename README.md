044135b2e600b269a0be8db44dd7ca4868665ba9
Amine2026 <mohamedamine.lazzez@esprit.tn>
2025-03-06 14:19:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# TuniParts Project
+
+## Documentation succincte
+
+TuniParts is a web project aimed at improving the online experience of buying automotive parts in Tunisia. It provides a user-friendly interface to browse and purchase car parts with real-time delivery tracking and a unique auction experience.
+
+## Team
+
+We are a team of 5 members, each responsible for two interfaces:
+
+- **Hamza**: Worked on Delivery and Tracking Order interfaces.
+- **Oussama Kaddech**:Pages: Login / sign up / contact
+- **Ali**: Worked on the product list and details interfaces.
+- **Souhail**: Worked on the Admin Dashboard and Order Management interfaces.
+- **Amine**: Worked on the Admin Dashboard and Order Management interfaces.
+
+## Instructions pour exécuter le projet
+
+### Prérequis
+
+Assurez-vous d'avoir installé [Node.js](https://nodejs.org/) et [npm](https://www.npmjs.com/) sur votre machine.
+
+### Étapes d'installation
+
+1. Clonez ce dépôt sur votre machine locale :
+  `git clone https://github.com/your-username/tuniparts.git`
+
+2. Accédez au répertoire du projet :
+   
+  `cd tuniparts`
+
+3. Installez les dépendances du projet avec npm :
+   
+  `npm install`
+
+4. Une fois les dépendances installées, construisez le projet avec la commande suivante :
+   
+  `npm run build`
+
+5. Vous pouvez maintenant lancer le projet localement en exécutant :
+   
+  `npm start`
+
+Deployment
+The project is set up for deployment on Vercel using GitHub integration. Once your project is pushed to GitHub, you can deploy it to Vercel easily by linking your GitHub repository to Vercel.
---
+++ tailwind.config.js
@@ -0,0 +1,52 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'Primary': '#CD170B',
+        'Secondary': '#010035',
+        'Background': '#D6D6D6',
+        'Available': '#42840F',
+        'NightBlue': '#010035',
+        'Prime': '#CD170B',
+        'GreyProduct': '#E6E6E6',
+        'GreytableProduct': '#D6D6D6',
+        'LightGreytableProduct': '#ECECEC',
+        'GreyRectangleProduct': '#EEEEEE',
+        'HeaderColor': '#E6E6E6',
+        'HeaderColor2': '#010035',
+        'InputBox': '#D9D9D9',
+        'GreyProduct': '#E6E6E6',
+        'GreytableProduct' : '#D6D6D6',
+        'LightGreytableProduct': '#ECECEC',
+        'GreyRectangleProduct': '#EEEEEE'
+      },
+      fontFamily: {
+        'Roboto': ['Roboto', 'sans-serif']
+      },
+      fontSize: {
+        mainTitle: ["32px", { fontWeight: 700 }],
+        footerText: ["32px", { fontWeight: 500 }],
+        headerText2: ["28px", { fontWeight: 500 }],
+        sectionTitle: ["24px", { fontWeight: 700 }],
+        cartTotal: ["20px", { fontWeight: 700 }],
+        subheading: ["20px", { fontWeight: 500 }],
+        profileText: ["20px", { fontWeight: 400 }],
+        cartsubheading: ["18px", { fontWeight: 700 }],
+        cart18: ["18px", { fontWeight: 500 }],
+        contactusbody: ["16px", { fontWeight: 500 }],
+        body: ["16px", { fontWeight: 400 }],
+        small: ["14px", { fontWeight: 300 }],
+      },
+    },
+  },
+  plugins: [],
+}
+
+//hetha lezem nhouto fih ay haja tet3awed
+//heberger site 3al vercel
+
